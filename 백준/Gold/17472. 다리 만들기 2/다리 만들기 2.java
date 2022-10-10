b2a846654a413ecf4a47adfb37439082accff514
Dodamon <dodamond222@gmail.com>
2022-10-11 00:21:53 +0900

    [Gold I] Title: 다리 만들기 2, Time: 76 ms, Memory: 11768 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -82,15 +81,13 @@ public class Main {
 		int count = 0;
 		while(!pq.isEmpty()) {
 			Edge edge = pq.poll();
-			//System.out.println(edge.v + " " + edge.u + " :: " + edge.cost );
 			if(!union(edge.v, edge.u)) {
 				total += edge.cost;
 				count++;
 			}
 			if(count == N-1) break;
 		}
-		
-		//System.out.println(Arrays.toString(parent));
+        
 		for(int i = 3; i < parent.length - 2; i++) {
 			if(!union(2, i)) {
 				total = 0;
@@ -119,7 +116,7 @@ public class Main {
 		}
 		/*------------------입력끝-------------------*/
 		
-		// 섬들에 숫자 표시 2 ~
+		//Step 1 :  BFS를 사용해서 섬들에 숫자를 표시한다 2 ~
 		int num = 2;
 		boolean[][] visited = new boolean[N][M];
 		for(int i = 0; i < N; i++) {
@@ -129,19 +126,18 @@ public class Main {
 				}
 			}
 		}
-		//System.out.println(Arrays.deepToString(map));
 		
 		
+		// Step 2 : 우선순위 큐에 만들수 있는 모든 다리의 종류를 넣는다
 		PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
 		int[] dy = {-1, 1, 0, 0};
 		int[] dx = {0, 0, -1, 1};
 		
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < M; j++) {
-				if(map[i][j] != 0) {
+				if(map[i][j] != 0) { // 섬이 있다면 4방향 탐색을 통해 놓을 수 있는 모든 다리를 우선순위 큐에 넣는다
 				
-					//System.out.println(y + " " + x);
-					for(int k = 0; k < 4; k++) {
+					for(int k = 0; k < 4; k++) { // 4방향을 탐색한다 상하좌우
 						int cost = 0;
 						int y = i;
 						int x = j;
@@ -151,17 +147,16 @@ public class Main {
 							int ny = y + dy[k];
 							int nx = x + dx[k];
 							
-							if(ny < 0 || ny >= N || nx < 0 || nx >= M || map[ny][nx] == n) break;
+							if(ny < 0 || ny >= N || nx < 0 || nx >= M || map[ny][nx] == n) break; // map의 범위를 벗어났거나 자신과 같은 섬에 닿았을 경우 반복을 멈춘다
 							
-							if(map[ny][nx] != 0 && map[ny][nx] != n) {
-								//System.out.println("here");
-								if(cost > 1) {
-									//System.out.println(i + " " + j + " "  + cost);
+							if(map[ny][nx] != 0 && map[ny][nx] != n) { // 다른 섬을 만났을 때
+								if(cost > 1) { // 다리의 길이가 1보다 크다면 -> 우선순위큐에 넣는다
 									pq.offer(new Edge(n, map[ny][nx], cost));
 								}
 								break;
 							}
 							
+							// 다른 섬에 닿지 못하고 아직 바다인 경우 같은 방향으로 계속 다리를 만든다.
 							y = ny;
 							x = nx;
 							cost++;
@@ -171,13 +166,32 @@ public class Main {
 			}
 		}
 		
-		parent = new int[num + 2];
-		for(int i = 2; i < num; i++ ) {
+		
+		// Step 3: Kruskal MST 알고리즘에서 새로 추가할 다리의 양쪽 섬이 같은 집합에 속해 있는 지 확인한다.
+		parent = new int[num + 2]; 
+		for(int i = 2; i < num; i++ ) { // 부모노드를 초기화한다
 			parent[i] = i;
 		}
 		
-		int result = kruskal(pq, num);
-		result = result == 0? -1: result;
+		int result = 0;
+		int count = 0;
+		while(!pq.isEmpty()) { // 다리 길이가 짧은 순서로 poll을 한다
+			Edge edge = pq.poll();
+			if(!union(edge.v, edge.u)) { // 다리의 양쪽 섬이 같은 집함에 속하지 앟으면 다리를 추가한다
+				result += edge.cost;
+				count++;
+			}
+			if(count == num-1) break; 
+		}
+        
+		for(int i = 3; i < parent.length - 2; i++) { // 모든 섬들이 연결되어있는지 확인한다
+			if(!union(2, i)) { 
+				result = 0;
+				break;
+			}
+		}
+
+		result = result == 0? -1: result; // 모든 섬이 연결이 안됐다면 -1을 연결됐으면 전체 다리의 길이를 출력한다
 		
 		/*---------------출력----------------*/
 		bw.write(result + "");
