b749d15e33604552a81299b56bf5ddbc2c629c7c
Dodamon <dodamond222@gmail.com>
2023-07-07 15:54:55 +0900

    [Silver III] Title: 이전 순열, Time: 44 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Silver/10973. 이전 순열/이전 순열.py
+++ 백준/Silver/10973. 이전 순열/이전 순열.py
@@ -1,18 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-def next_permutation(nums, N) -> list:
+def pre_permutation(nums, N) -> list:
 
     # 바뀌는 순열 인덱스를 찾는다
+    # 뒤에서 부터 내림차순이 아닌 인덱스 i를 찾는다
     i = N-1
     while i > 0 and nums[i-1] <= nums[i]: i -= 1
     if i == 0: return {-1}
 
+    # 뒤에서 부터 nums[i] 보다 작은 수 가 나오는 인덱스 j를 찾는다
     j = N-1
     while j > i-1 and nums[i-1] <= nums[j]: j -= 1
 
 
-    # swap
+    # swap(i-1, j)
     nums[i-1], nums[j] = nums[j], nums[i-1]
     return nums[:i] + sorted(nums[i:], reverse=True)
 
@@ -20,7 +22,7 @@ def next_permutation(nums, N) -> list:
 def solution() -> None:
     N = int(input())
     nums = list(map(int, input().split()))
-    print(*next_permutation(nums, N), sep=" ")
+    print(*pre_permutation(nums, N), sep=" ")
 
 
 solution()
