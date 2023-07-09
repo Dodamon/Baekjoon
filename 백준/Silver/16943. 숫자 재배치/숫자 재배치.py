84edb8b8c50e63ec5ae1cb0bddac0bb92826a681
Dodamon <dodamond222@gmail.com>
2023-07-09 20:44:58 +0900

    [Silver I] Title: 숫자 재배치, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16943. 숫자 재배치/숫자 재배치.py
+++ 백준/Silver/16943. 숫자 재배치/숫자 재배치.py
@@ -1,25 +1,23 @@
+def dfs(depth, visit, now):
+    global ans
+    if depth == L:
+        ans = now
+        return True
+    
+    for i in range(L):
+        if visit & (1 << i):
+            continue
+        new = now + A[i] * 10 ** (L-1-depth)
+        if 0 < new < B:
+            if dfs(depth+1, visit | (1 << i), new):
+                return True
+    return False
+
 A, B = input().split()
 A = sorted(map(int, A), reverse=True)
 B = int(B)
 L = len(A)
+
 ans = -1
-
-def dfs(depth, visited, cur) -> bool:
-    global ans
-
-    if depth == L:
-        ans = cur
-        return True
-    
-    for i in range(L):
-        if visited & (1 << i):
-            continue
-
-        new = cur * 10 + A[i]
-        if 0 < new < B:
-            if dfs(depth+1, visited | (1 << i), new):
-                return True
-
-
 dfs(0, 0, 0)
 print(ans)
