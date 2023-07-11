0cc0146e15682ebe60d5f97126f85f92624524b1
Dodamon <dodamond222@gmail.com>
2023-07-11 17:57:44 +0900

    [level 2] Title: 짝지어 제거하기, Time: 220.31 ms, Memory: 14.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,12 +1,15 @@
 def solution(s):
     answer = 0
     stack = []
-
+    
+    # 문자길이가 1이거나 짝수가 아닐때는 짝을 지어 제거할수 없기 때문에 바로 출력한다
     if len(s) == 1 or len(s) % 2 == 1:
         return answer
     
+    # 문자열을 하나씩 돌면서
+    # 스텍의 top과 같으면 스텍에서 pop 한다
+    # 스텍의 top과 다르면 스텍에 push 한다
     for cur in s:
-
         if len(stack) == 0:
             stack.append(cur)
             continue
@@ -17,6 +20,7 @@ def solution(s):
         else:
             stack.append(cur) 
     
+    # 스텍이 비어있다는 것은 모든 문자열을 제거했다는 뜻이다 
     if len(stack) == 0:
         answer = 1
     return answer
