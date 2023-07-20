bb04a65ed965f8d29f8a9df03413359b9432d7ea
Dodamon <dodamond222@gmail.com>
2023-07-21 00:58:20 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,3 +1,28 @@
 def solution(s):
-    answer = 0
+    answer = len(s)
+
+    for l in range(1, len(s)//2+1):
+        stack = []
+
+        count = 1
+        result = 0
+        for i in range(0, len(s), l):
+            word = s[i: i+l]
+            if len(stack) == 0:
+                stack.append(word)
+                result += l
+            else:
+                if stack[-1] == word:
+                    count += 1
+                else:
+                    if count != 1:
+                        result += len(str(count))
+                    count = 1
+                    result += len(word)
+                    stack.append(word)
+                    
+        if count != 1:
+            result += len(str(count))
+
+        answer = min(result, answer)
     return answer
