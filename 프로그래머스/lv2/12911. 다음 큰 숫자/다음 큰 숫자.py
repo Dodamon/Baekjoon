ab264d491286896e6b34d7f19776151ffdc087d2
Dodamon <dodamond222@gmail.com>
2023-07-22 14:50:41 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -0,0 +1,27 @@
+def next_permutation(s):
+    i = len(s) - 1
+    while i > 0 and s[i - 1] >= s[i]:
+        i -= 1
+
+    if i == 0:
+        return "".join(s)
+
+    j = len(s) - 1
+    while s[i - 1] >= s[j]:
+        j -= 1
+
+    s[i - 1], s[j] = s[j], s[i - 1]
+    return "".join(s[:i] + sorted(s[i:]))
+
+
+def solution(n):
+    binary = list(bin(n)[2:])
+    answer = int("0b" + next_permutation(binary), 2)
+
+    if answer == n:
+        if all(n == "1" for n in binary):
+            answer = int("0b10" + ("1" * (len(binary) - 1)), 2)
+        else:
+            answer = int("0b10" + "".join(sorted(binary[1:])), 2)
+    return answer
+
