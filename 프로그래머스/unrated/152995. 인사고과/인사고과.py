7f519876bcadd8ee1cfe654d240395bbd7fb4b50
Dodamon <dodamond222@gmail.com>
2023-07-24 17:37:33 +0900

    [unrated] Title: 인사고과, Time: 183.83 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/unrated/152995. 인사고과/인사고과.py
+++ 프로그래머스/unrated/152995. 인사고과/인사고과.py
@@ -1,26 +1,30 @@
 def solution(scores):
+    answer = 0
+    count = 0
+    total = 0
+    grade = 1
 
-    wanho, scores = scores[0], scores[1:]
-    wanho_a, wanho_b = wanho
-    threshold = 0
-    rank = 1
+    y, x = sorted(scores, key=lambda x: (x[0]+1) * (x[1]+1), reverse=True)[0]
+    scores[:] = sorted(list(zip(scores, [i for i in range(len(scores))])), key=lambda x: x[0][0] + x[0][1], reverse=True)
 
-    # score_a는 내림차순 score_b는 오름차순 기준으로 정렬해서 -> 이렇게 되면 b만 비교할 수 있다.
-    # score_a기준으로 연산을 진행한다
-    # 1. 원후의 a, b가 현재 score 보다 작다면 바로 -1을 리턴한다
+    for score in scores:
+        (ny, nx), ni = score
 
-    # 2. 만약 threshold 값보다 b의 값이 같거나 클 경우 => threshold 값을 갱신한다.
-    # 3. 만약 threshold 값보다 b의 값이 작을 경우 => threshold의 a보다 현재 a값이 무조건 작거나 같은데 b까지 작으면 
-    # rank에 영향을 주지 않고 만약 a가 작을 경우 인센티브를 받을 수 없다
-    for score_a, score_b in sorted(scores, key=lambda s: (-s[0], s[1])):
+        if ny < y and nx < x:
+            if ni == 0:
+                answer = -1
+        else:
+            if ny + nx == total:
+                count += 1
+            else:
+                grade += count
+                total = ny + nx
+                count = 1
 
-        if wanho_a < score_a and wanho_b < score_b:
-            return -1
+        if ni == 0:
+            break
 
-        if threshold <= score_b:
-            threshold = score_b
+    if answer != -1:
+        answer = grade
 
-            if wanho_a + wanho_b < score_a + score_b:
-                rank += 1
-    return rank
-
+    return answer
