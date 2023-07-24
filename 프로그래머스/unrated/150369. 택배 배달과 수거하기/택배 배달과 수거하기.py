c417e5a64ebcaac6317d00db2cddf3ca17b2468a
Dodamon <dodamond222@gmail.com>
2023-07-24 11:04:07 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 63.97 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -4,11 +4,15 @@ def solution(cap, n, deliveries, pickups) -> int:
 
     truck_to_deli = 0
     truck_to_pick = 0
-
+    
+    # 스택을 사용해서 뒤에서 꺼내면서
+    # 트럭의 박스 수에 최대 허용량 cap을 빼준다
+    # 트럭의 박스 수가 양수가 될때 트럭이 가득찬것이기 때문에 출발지점으로 돌아가야한다.
     while stack:
         deli, pick = stack.pop()
         truck_to_pick += pick
         truck_to_deli += deli
+        
 
         while truck_to_pick > 0 or truck_to_deli > 0:
             truck_to_pick -= cap
