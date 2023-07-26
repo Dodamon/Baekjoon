aa3d98cdc8a61ce25bf5a69a1cc086781abfd11e
Dodamon <dodamond222@gmail.com>
2023-07-26 16:54:21 +0900

    [unrated] Title: 귤 고르기, Time: 15.48 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,19 +1,14 @@
 import collections
 
+
 def solution(k, tangerine):
     answer = 0
-    counts = collections.defaultdict(int)
+    counts = collections.Counter(tangerine)
     # 같은 종류의 귤끼리 count를 한다
     # 가장 count가 높은 귤 순으로 박스에 넣는다
-
-    for t in tangerine:
-        counts[t] += 1
-    counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
-
-    for count in counts:
+    for count in sorted(counts.values(), reverse=True):
         if k <= 0:
             break
         answer += 1
-        k -= count[1]
-
+        k -= count
     return answer
