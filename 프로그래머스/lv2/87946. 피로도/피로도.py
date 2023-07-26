d62cc7748763bdc3d6b57c08fa7a1b0c39c7a020
Dodamon <dodamond222@gmail.com>
2023-07-26 16:22:26 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -4,16 +4,22 @@ def solution(k, dungeons):
 
     def dfs(cur, visited, cnt):
         global answer
-
+        
+        # 만약에 모든 던전수를 갈 수 있는 방법이 나왔다면 바로 탐색을 끝낸다
         if cnt == len(dungeons):
             answer = cnt
             return True
-        answer = max(cnt, answer)
 
+        # 가장 많이 방문 할 수 있는 던전수를 갱신한다
+        answer = max(cnt, answer)
+        
+        # for문을 통해 탐색하면서 갈수 있는 던전을 방문한다
         for i, dungeon in enumerate(dungeons):
+            # 방문하지 못하는 던전이면 탐색을 계속한다
             if visited[i] or dungeon[0] > cur:
                 continue
-
+            
+            # 방문 처리
             visited[i] = True
             if dfs(cur - dungeon[1], visited, cnt + 1):
                 return True
