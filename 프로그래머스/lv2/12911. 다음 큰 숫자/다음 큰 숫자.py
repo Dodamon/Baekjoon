83d2035a00045d1b7cabb46729f602816a9506ea
Dodamon <dodamond222@gmail.com>
2023-07-26 10:23:39 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,4 +1,5 @@
 def next_permutation(s):
+    # next permutation을 할때는 for 문이 아닌 while 문을 사용한다 
     i = len(s) - 1
     while i > 0 and s[i - 1] >= s[i]:
         i -= 1
@@ -17,7 +18,10 @@ def next_permutation(s):
 def solution(n):
     binary = list(bin(n)[2:])
     answer = int("0b" + next_permutation(binary), 2)
-
+    
+    # next permutation을 이용해서 다음 오는 수중 가장 큰수를 구한다
+    # 만약 nex permuatation으로 만든 숫자와 처음 숫자가 같다면 '0'을 추가하고 그 이후를 오름차순으로 정렬한다.
+    
     if answer == n:
         answer = int("0b10" + "".join(sorted(binary[1:])), 2)
     return answer
