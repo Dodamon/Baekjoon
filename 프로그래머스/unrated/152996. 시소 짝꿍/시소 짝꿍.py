617787deee7bcf27907f670c65bdf9fd43fbcdd9
Dodamon <dodamond222@gmail.com>
2023-07-29 16:35:08 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -0,0 +1,15 @@
+import collections
+
+def solution(weights):
+    answer = 0
+    # 1:1
+    # 1:2
+    # 2:3
+    # 3:4
+    dict = collections.defaultdict(int)
+    
+    for w in weights:
+        answer += dict[w] + dict[w*2] + dict[w/2] + dict[w*3/2] + dict[w*2/3] + dict[w*4/3] + dict[w*3/4]
+        dict[w] += 1
+    
+    return answer
