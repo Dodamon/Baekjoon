a091bbff2453a53387d61707fdc98c6cd6e61ab5
Dodamon <dodamond222@gmail.com>
2023-07-31 18:40:59 +0900

    Time: 28 ms (97.55%), Space: 14.7 MB (10.12%) - LeetHub

---
+++ LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        index = defaultdict(lambda : -1)
+        dp = [1 for _ in range(len(s))]
+        left, right = 0, 0
+        
+        if not s:
+            return 0
+
+        for i, char in enumerate(s):
+            if i == 0:
+                index[char] = i
+                continue
+
+            if index[char] != -1:
+                for l in range(left, index[char]):
+                    index[s[l]] = -1
+                left = index[char] + 1
+
+            index[char] = i
+            right += 1
+            dp[i] = right - left + 1
+            
+        return max(dp)
