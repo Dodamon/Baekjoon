ebc67f970356964b5c286dea970f520817dd22e3
Dodamon <dodamond222@gmail.com>
2023-08-06 15:06:15 +0900

    [level 3] Title: 야근 지수, Time: 394.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.py
@@ -10,6 +10,10 @@ def solution(n, works):
     if total - len(works) == n:
         return len(works)
     
+    # 우선 순위 큐에 넣어서 작업량이 많은 순으로 작업을 pop한다.
+    # 작업에서 1씩 뺀다음 다시 우선순위 큐에 넣는다.
+    # 시간 복잡도 nlogn
+    
     for work in works:
         heapq.heappush(pq, work*-1)
         
