83fd70eba3883336994a2fd176e29fce04195618
Dodamon <dodamond222@gmail.com>
2023-08-07 17:24:00 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 66.38 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/README.md
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 쿼드압축 후 개수 세기 - 68936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936?language=python3) 
 
 ### 성능 요약
 
-메모리: 12.1 MB, 시간: 59.25 ms
+메모리: 12.2 MB, 시간: 66.38 ms
 
 ### 구분
 
--- 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -2,10 +2,14 @@ def solution(arr):
     global answer
     answer = [0, 0]
     
+    # 재귀로 풀수 있다
+    # y: 쿼드 압축을 시작하는 박스의 가장 왼쪽 위 y좌표
+    # x: 퀴드 압축을 시작하는 박스의 가장 왼쪽 위 x좌표
     def compress(y, x, n):
         state = arr[y][x]
         isPossible = True
         
+        # 한가지 상태로 이뤄져 있는지 탐색하낟
         for i in range(y, y + n):
             for j in range(x, x + n):
                 if arr[i][j] != state:
@@ -14,6 +18,8 @@ def solution(arr):
             if not isPossible:
                 break;
         
+        # 하나의 상태로 나타낼수 있다면 바로 answer에 값을 저장한다
+        # 하나의 상태로 나타낼수 없다면 재귀적으로 탐색을 시작한다
         if isPossible:
             answer[state] += 1
         else:
