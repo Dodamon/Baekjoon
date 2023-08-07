56528d53a58570110fa2f44ef55f5960f30ec457
Dodamon <dodamond222@gmail.com>
2023-08-07 18:22:12 +0900

    [level 3] Title: 단속카메라, Time: 2.44 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -3,6 +3,9 @@ def solution(routes):
     answer = 1
     cmp = routes[0][1]
     
+    # 차량의 진출 시점을 기준으로 정렬을 한다
+    # 현재 차량의 진입 시점이 앞의 차량의 진출 시점보다 작다면 겹치는 구간이 생기기 때문에
+    # 하나의 단속카메라만을 둘수 있다
     for i in range(1, len(routes)):
         start, end = routes[i]
         if start > cmp:
