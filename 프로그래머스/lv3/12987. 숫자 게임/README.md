acdb603b09de7275855a3b83ef45dff056b229fa
Dodamon <dodamond222@gmail.com>
2023-08-07 16:43:46 +0900

    [level 3] Title: 숫자 게임, Time: 56.94 ms, Memory: 18.2 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/README.md
+++ 프로그래머스/lv3/12987. 숫자 게임/README.md
@@ -1,10 +1,10 @@
 # [level 3] 숫자 게임 - 12987 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12987) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12987#) 
 
 ### 성능 요약
 
-메모리: 18.3 MB, 시간: 59.54 ms
+메모리: 18.2 MB, 시간: 56.94 ms
 
 ### 구분
 
--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
@@ -1,11 +1,16 @@
 def solution(A, B):
     answer = 0
     
+    # 오름 차순으로 정렬한다
+    # 정렬해서 순서를 바꿔도 되는 이유는
+    # A의 순서를 임의로 만들고 B를 맞추면 되기 때문이다
     A.sort()
     B.sort()
     
     j = 0
     for i, a in enumerate(A):
+        # 현재 a는 A중 가장 작은 수다
+        # B에서 작은수중에서 a보다 큰수를 찾아준다.
         while j < len(B) and a >= B[j]:
             j += 1
         
