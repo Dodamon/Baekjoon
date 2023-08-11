b4e0fafb6ebcb576957b6727f2c4e4f810cd07d0
Dodamon <dodamond222@gmail.com>
2023-08-11 23:07:46 +0900

    [Silver II] Title: 창고 다각형, Time: 296 ms, Memory: 20584 KB -BaekjoonHub

---
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Main {
+
+    public static class Storage implements Comparable<Storage>{
+        private int x;
+        private int y;
+
+        public Storage(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Storage o) {
+            if (this.x == o.x) {
+                return this.y - o.y;
+            }
+            return this.x - o.x;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        List<Storage> storages = new ArrayList<>();
+
+        for(int i = 0; i < N; i++) {
+            storages.add(new Storage(sc.nextInt(), sc.nextInt()));
+        }
+        Collections.sort(storages);
+
+        int left = 0;
+        int right = N-1;
+        Storage leftMax = new Storage(0, 0);
+        Storage rightMax = new Storage(0, 0);
+
+        int answer = 0;
+        while (left <= right) {
+            if (leftMax.y <= storages.get(left).y){
+                answer += leftMax.y * (storages.get(left).x - leftMax.x);
+                leftMax.y = storages.get(left).y;
+                leftMax.x = storages.get(left).x;
+            }
+
+            if (rightMax.y <= storages.get(right).y) {
+                answer += rightMax.y * (rightMax.x - storages.get(right).x);
+                rightMax.y = storages.get(right).y;
+                rightMax.x = storages.get(right).x;
+            }
+
+            if (storages.get(left).y < storages.get(right).y) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        answer += storages.get(left).y;
+        System.out.println(answer);
+    }
+}
