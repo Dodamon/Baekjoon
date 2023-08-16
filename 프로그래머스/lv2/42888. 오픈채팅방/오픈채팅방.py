f9adf87c0b7e47909748fd1418d79d239d141965
Dodamon <dodamond222@gmail.com>
2023-08-16 11:51:09 +0900

    [level 2] Title: 오픈채팅방, Time: 69.82 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,28 @@
+# 해시
+def solution(record):
+    answer = []
+    names = {}
+    
+    # 기록을 읽으면서 id당 닉네임을 저장하고 바꾼다
+    for r in record:
+        records = r.split()
+        id = records[1]
+        op = records[0]
+        if op == "Enter":
+            nick = records[2]
+            names[id] = nick
+        elif op == "Change":
+            nick = records[2]
+            names[id] = nick  
+            
+    # 정답을 저장한다
+    for r in record:
+        records = r.split()
+        id = records[1]
+        op = records[0]
+        if op == "Enter":
+            answer.append(f"{names[id]}님이 들어왔습니다.")
+        elif op == "Leave":
+            answer.append(f"{names[id]}님이 나갔습니다.")           
+            
+    return answer
