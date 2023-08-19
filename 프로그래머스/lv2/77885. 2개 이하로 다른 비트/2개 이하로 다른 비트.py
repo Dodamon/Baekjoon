6f88c85f848d8e1b0977d5922914176c4102975d
Dodamon <dodamond222@gmail.com>
2023-08-19 16:39:07 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 471.66 ms, Memory: 23.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+def solution(numbers):
+    answer = []
+    
+    for n, num in enumerate(numbers):
+        
+        # 한개의 비트만 달라질때
+        # 일의자리 부터 탐색해서 0 이 있다면 넣어주고 탐색을 멈춘다
+        i = 0
+        N = len(bin(num)) - 2
+        while i < N:
+            if num & (1 << i) == 0:
+                num |= (1 << i)
+                break
+            i += 1
+        
+        # 두개의 비트가 달라질때
+        if num == numbers[n]:
+            num |= (1 << i)
+            num ^= (1 << i-1)
+        else:
+            j = i-1
+            while j >= 0:
+                if num & (1 << j) != 0:
+                    num ^= (1 << j)
+                    break
+                j -= 1
+            
+        # 정답배열에 넣는다    
+        answer.append(num)
+    return answer
