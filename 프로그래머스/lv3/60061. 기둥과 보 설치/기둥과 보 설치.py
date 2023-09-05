f5a0c5bcfe1d26008258843a12ca374f89d17e6a
Dodamon <dodamond222@gmail.com>
2023-09-05 18:43:54 +0900

    [level 3] Title: 기둥과 보 설치, Time: 1.20 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/60061. 기둥과 보 설치/기둥과 보 설치.py
+++ 프로그래머스/lv3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -8,29 +8,19 @@ def solution(n, build_frame):
         # 여기 보가 있는게 맞다면
         
         # 1. 보 아래에 기둥이 있거나
-        if hashmap[(x, y-1, 0)] == 1:
-            return True
         # 2. 보 오른쪽 아래에 기둥이 있거나
-        if hashmap[(x+1, y-1, 0)] == 1:
-            return True
         # 3. 보 양쪽에 보가 있거나
-        if hashmap[(x-1, y, 1)] == 1 and hashmap[(x+1, y, 1)] == 1:
+        if hashmap[(x, y-1, 0)] or hashmap[(x+1, y-1, 0)] or ( hashmap[(x-1, y, 1)] and hashmap[(x+1, y, 1)]):
             return True
         return False
         
     def checkCol(x, y) -> bool:
         # 여기 기둥이 있는게 맞다면
         # 1. 아래가 바닥이거나
-        if y == 0:
-            return True
         # 2. 왼쪽이 보이거나
-        if hashmap[(x-1, y, 1)] == 1:
-            return True
         # 3. 오른쪽이 보이거나
-        if hashmap[(x, y, 1)] == 1:
-            return True
-        # 3. 아래가 기둥이다
-        if hashmap[(x, y-1, 0)] == 1:
+        # 4. 아래가 기둥이다
+        if y == 0 or hashmap[(x-1, y, 1)] or hashmap[(x, y, 1)] or hashmap[(x, y-1, 0)]:
             return True
         return False
     
@@ -46,44 +36,37 @@ def solution(n, build_frame):
         # 기둥삭제
         elif b == 0 and a == 0:
             hashmap[(x, y, 0)] = 0
-            if y+1 <= n and hashmap[(x, y+1, 0)] == 1:
-                if not checkCol(x, y+1):
-                    hashmap[(x, y, 0)] = 1
-                    continue
+            if y+1 <= n and hashmap[(x, y+1, 0)] == 1 and not checkCol(x, y+1):
+                hashmap[(x, y, 0)] = 1
+                continue
+                   
+            if y+1 <= n and hashmap[(x, y+1, 1)] == 1 and not checkRow(x, y+1):
+                hashmap[(x, y, 0)] = 1
+                continue
                     
-            if y+1 <= n and hashmap[(x, y+1, 1)] == 1:
-                if not checkRow(x, y+1):
-                    hashmap[(x, y, 0)] = 1
-                    continue
-                    
-            if y+1 <= n and x-1 >= 0 and hashmap[(x-1, y+1, 1)] == 1:
-                if not checkRow(x-1, y+1):
-                    hashmap[(x, y, 0)] = 1
-                    continue
+            if y+1 <= n and x-1 >= 0 and hashmap[(x-1, y+1, 1)] == 1 and not checkRow(x-1, y+1):
+                hashmap[(x, y, 0)] = 1
+                continue
             
         # 보오삭제
         elif b == 0 and a == 1:
             hashmap[(x, y, 1)] = 0
             
-            if hashmap[(x, y, 0)] == 1:
-                if not checkCol(x, y):
-                    hashmap[(x, y, 1)] = 1
-                    continue
+            if hashmap[(x, y, 0)] == 1 and not checkCol(x, y):
+                hashmap[(x, y, 1)] = 1
+                continue
                     
-            if x+1 <= n and hashmap[(x+1, y, 0)] == 1:
-                if not checkCol(x+1, y):
-                    hashmap[(x, y, 1)] = 1
-                    continue
+            if x+1 <= n and hashmap[(x+1, y, 0)] == 1 and not checkCol(x+1, y):
+                hashmap[(x, y, 1)] = 1
+                continue
                     
-            if x+1 <= n and hashmap[(x+1, y, 1)] == 1:
-                if not checkRow(x+1, y):
-                    hashmap[(x, y, 1)] = 1
-                    continue
+            if x+1 <= n and hashmap[(x+1, y, 1)] == 1 and not checkRow(x+1, y):
+                hashmap[(x, y, 1)] = 1
+                continue
                     
-            if x-1 >= 0 and hashmap[(x-1, y, 1)] == 1:
-                if not checkRow(x-1, y):
-                    hashmap[(x, y, 1)] = 1
-                    continue
+            if x-1 >= 0 and hashmap[(x-1, y, 1)] == 1and not checkRow(x-1, y):
+                hashmap[(x, y, 1)] = 1
+                continue
 
                 
     for key in sorted(hashmap.keys()):
