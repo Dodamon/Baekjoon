f09ec29d361e573fee1c4146653a798fc34ae6d3
Dodamon <dodamond222@gmail.com>
2023-09-24 00:01:55 +0900

    [unrated] Title: 숫자 변환하기, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -4,6 +4,7 @@ def solution(x, y, n):
     answer = 0
     queue = deque()
     queue.append((y, 0))
+    visited = defaultdict(bool)
     if x == y:
         return 0
     
@@ -15,11 +16,14 @@ def solution(x, y, n):
         if num < x:
             continue
         
-        if num % 2 == 0:
+        if num % 2 == 0 and not visited[num//2]:
+            visited[num//2] = True
             queue.append((num//2, depth+1))
         if num % 3 == 0:
+            visited[num//3] = True
             queue.append((num//3, depth+1))
         if num > n:
+            visited[num-n] = True
             queue.append((num-n, depth+1))
     
     if answer == 0:
