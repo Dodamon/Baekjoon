c12f82e4c43c86268e377cde2a2efb52b4f5a120
Dodamon <dodamond222@gmail.com>
2023-10-03 23:55:45 +0900

    [level 2] Title: 피로도, Time: 0.12 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,32 +1,19 @@
+from itertools import permutations
+
 def solution(k, dungeons):
-    global answer
     answer = -1
-
-    def dfs(cur, visited, cnt):
-        global answer
-        
-        # 만약에 모든 던전수를 갈 수 있는 방법이 나왔다면 바로 탐색을 끝낸다
-        if cnt == len(dungeons):
-            answer = cnt
-            return True
-
-        # 가장 많이 방문 할 수 있는 던전수를 갱신한다
+    # python의 내장함수를 사용하는게 더 빠르다
+    # n이 충분히 작아서 가능하다
+    perms = permutations(dungeons, len(dungeons))
+    
+    for perm in perms:
+        cur = k
+        cnt = 0
+        for dungeon in perm:
+            if cur >= dungeon[0]:
+                cur -= dungeon[1]
+                cnt += 1
         answer = max(cnt, answer)
-        
-        # for문을 통해 탐색하면서 갈수 있는 던전을 방문한다
-        for i, dungeon in enumerate(dungeons):
-            # 방문하지 못하는 던전이면 탐색을 계속한다
-            if visited[i] or dungeon[0] > cur:
-                continue
-            
-            # 방문 처리
-            visited[i] = True
-            if dfs(cur - dungeon[1], visited, cnt + 1):
-                return True
-            visited[i] = False
-
-        return False
-
-    dfs(k, [False for _ in range(len(dungeons))], 0)
+        if answer == len(dungeons):
+            break
     return answer
-
